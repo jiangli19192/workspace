fde5f4d3b5f074b27dec362832165adb1410da27
zhangjl27 <zhangjiangli19192@163.com>
2021-01-12 19:30:35 +0800

    Add 新的拆分List测试用例

--- javaBaseStudy/src/com/zjl/study/list/ListSplitTest.java
+++ javaBaseStudy/src/com/zjl/study/list/ListSplitTest.java
@@ -1,6 +1,9 @@
 package com.zjl.study.list;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * link url:https://blog.csdn.net/weixin_41848505/article/details/94380420
@@ -12,7 +15,36 @@ import java.util.List;
 public class ListSplitTest {
  
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList();
+//        splitMultiList();
+        splitMultiListApplyJava8();
+    }
+    
+    /**
+     * 将一个size大的List集合平均分配成两个List集合
+     * 能被2整除，则两个集合的长度相同
+     * 不能被2整除，则（list.size() / 2）个长度的放到第一个list,剩余的数据放到第二个List内
+     */
+    private static void splitMultiListApplyJava8() {
+    	List<String> list = new ArrayList();
+    	for (int i = 1; i <= 1988; i++) {
+            list.add(String.valueOf(1));
+        }
+    	
+    	
+    	int num = list.size() / 2;
+    	System.out.println("num=" + num);
+    	
+    	Set<String> firstList = list.stream().limit(num).collect(Collectors.toSet());
+    	list.removeAll(firstList);
+    	System.out.println("firstList size=" + firstList.size());
+    	System.out.println("otherList size=" + list.size());
+    	
+    }
+
+
+
+	private static void splitMultiList() {
+		List<Integer> list = new ArrayList();
         for (int i = 1; i <= 1988; i++) {
             list.add(i);
         }
@@ -21,7 +53,9 @@ public class ListSplitTest {
         	System.out.println(result.get(i));
         }
         System.out.println("分隔后List个数：\t" + result.size());
-    }
+	}
+    
+    
  
     /**
      * 按指定大小，分隔集合，将集合按规定个数分为n个部分
