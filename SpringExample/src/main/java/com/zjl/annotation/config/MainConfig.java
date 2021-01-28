6dd6336dbfa0ec1d70d20784e9d8581004d81614
jiangli19192 <zhangjiangli19192@163.com>
2021-01-29 00:08:54 +0800

    在包扫描注解上面，应用排除过滤属性

--- SpringExample/src/main/java/com/zjl/annotation/config/MainConfig.java
+++ SpringExample/src/main/java/com/zjl/annotation/config/MainConfig.java
@@ -2,7 +2,11 @@ package com.zjl.annotation.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
 import com.zjl.annotation.bean.Person;
 
@@ -14,7 +18,11 @@ import com.zjl.annotation.bean.Person;
  *
  */
 @Configuration // 告诉Spring这是一个配置类
-@ComponentScan(value = "com.zjl.annotation") // 开启包扫描配置，等同于配置文件中的<context:component-scan>标签
+@ComponentScan(value = "com.zjl.annotation", excludeFilters = {
+		@Filter(type = FilterType.ANNOTATION, classes = {Controller.class, Service.class})
+}) // 开启包扫描配置，等同于配置文件中的<context:component-scan>标签
+// ComponentScan value:指定要扫描的包
+// excludeFilters = Filter[]:指定扫描的时候，按照什么样的规则排除哪些组件
 public class MainConfig {
 
 	// 给容器中注册一个Bean;类型为返回值的类型，id默认是用方法名作为id
