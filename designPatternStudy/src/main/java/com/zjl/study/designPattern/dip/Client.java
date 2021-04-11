236874153857886c4930d35d8f6a709ca8885849
jiangli19192 <zhangjiangli19192@163.com>
2021-04-11 16:49:18 +0800

    改造成符合依赖倒置原则实现方式

--- designPatternStudy/src/main/java/com/zjl/study/designPattern/dip/Client.java
+++ designPatternStudy/src/main/java/com/zjl/study/designPattern/dip/Client.java
@@ -1,43 +1,51 @@
 package com.zjl.study.designPattern.dip;
 
+/**
+ * @author zjl
+ */
 public class Client {
     public static void main(String[] args) {
-        Book book = new Book();
+        IMaterial book = new Book();
+        IMaterial newsPaper = new NewsPaper();
         Mather mather = new Mather();
         mather.read(book);
+        mather.read(newsPaper);
     }
 }
 
-class Book {
+/**
+ * 抽象层
+ */
+interface IMaterial {
+    /**
+     * 返回读物里面的故事内容
+     *
+     * @return String
+     */
+    String getContent();
+}
+
+/**
+ * Book和NewsPaper,都属于低层依赖抽象层
+ */
+class Book implements IMaterial {
     public String getContent() {
         return "书的故事很精彩";
     }
 }
 
-class NewsPaper {
+class NewsPaper implements IMaterial {
     public String getContent() {
         return "报纸的故事很精彩";
     }
 }
 
+/**
+ * 高层依赖抽象层
+ */
 class Mather {
-    public void read(Book book) {
+    public void read(IMaterial material) {
         System.out.println("妈妈开始讲故事");
-        System.out.println(book.getContent());
-    }
-
-    /**
-     * 新增了NewsPaper类
-     * 妈妈就增加一个读报纸的方法。
-     * 扩展一个类，就修改妈妈这个类，这种实现违反了开闭原则。
-     *
-     * 出现此问题的原因：
-     *      高层依赖低层，Mather类依赖于具体的书或报纸
-     *
-     * @param newsPaper
-     */
-    public void readNewsPaper(NewsPaper newsPaper) {
-        System.out.println("妈妈开始讲故事");
-        System.out.println(newsPaper.getContent());
+        System.out.println(material.getContent());
     }
 }
