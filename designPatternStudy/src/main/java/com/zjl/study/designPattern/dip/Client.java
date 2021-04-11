d520d56ad25412931a652b23ae510aab538dc0a6
jiangli19192 <zhangjiangli19192@163.com>
2021-04-11 16:10:32 +0800

    添加妈妈只讲书里的故事

--- designPatternStudy/src/main/java/com/zjl/study/designPattern/dip/Client.java
+++ designPatternStudy/src/main/java/com/zjl/study/designPattern/dip/Client.java
@@ -1,4 +1,22 @@
 package com.zjl.study.designPattern.dip;
 
 public class Client {
+    public static void main(String[] args) {
+        Book book = new Book();
+        Mather mather = new Mather();
+        mather.read(book);
+    }
+}
+
+class Book {
+    public String getContent() {
+        return "书的故事很精彩";
+    }
+}
+
+class Mather {
+    public void read(Book book) {
+        System.out.println("妈妈开始讲故事");
+        System.out.println(book.getContent());
+    }
 }
