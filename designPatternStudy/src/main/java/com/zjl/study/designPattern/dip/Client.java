4a925d059ef69f1168ab736a44106a59be0249ec
jiangli19192 <zhangjiangli19192@163.com>
2021-04-11 16:30:51 +0800

    违反开闭原则的实现

--- designPatternStudy/src/main/java/com/zjl/study/designPattern/dip/Client.java
+++ designPatternStudy/src/main/java/com/zjl/study/designPattern/dip/Client.java
@@ -14,9 +14,30 @@ class Book {
     }
 }
 
+class NewsPaper {
+    public String getContent() {
+        return "报纸的故事很精彩";
+    }
+}
+
 class Mather {
     public void read(Book book) {
         System.out.println("妈妈开始讲故事");
         System.out.println(book.getContent());
+    }
+
+    /**
+     * 新增了NewsPaper类
+     * 妈妈就增加一个读报纸的方法。
+     * 扩展一个类，就修改妈妈这个类，这种实现违反了开闭原则。
+     *
+     * 出现此问题的原因：
+     *      高层依赖低层，Mather类依赖于具体的书或报纸
+     *
+     * @param newsPaper
+     */
+    public void readNewsPaper(NewsPaper newsPaper) {
+        System.out.println("妈妈开始讲故事");
+        System.out.println(newsPaper.getContent());
     }
 }
